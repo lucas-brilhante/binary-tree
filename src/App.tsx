101c01b50a0e87b2d53c910e82adf6a6d5b24100
lucas-brilhante <lucasbrilhantepires@gmail.com>
2023-06-09 15:04:30 -0300

    feat: add tree's remove function

--- src/App.tsx
+++ src/App.tsx
@@ -2,46 +2,16 @@ import { Graph } from "./components/graph";
 import { Header } from "./components/header";
 import { useBinaryTree } from "./hooks/use-binary-tree";
 
-const dummy = {
-  value: 8,
-  leftNode: {
-    value: 4,
-    leftNode: {
-      value: 2,
-      rightNode: {
-        value: 3,
-      },
-    },
-    //rightNode: {
-    //  value: 5,
-    //},
-  },
-  rightNode: {
-    value: 12,
-    leftNode: {
-      value: 10,
-      leftNode: {
-        value: 9,
-      },
-      rightNode: {
-        value: 11,
-      },
-    },
-    rightNode: {
-      value: 14,
-    },
-  },
-};
-
 function App() {
-  const { tree, addNode, removeNode, clearTree } = useBinaryTree({
-    initialState: dummy,
-  });
-  console.log("tree", tree);
+  const { tree, addNode, removeNodeByValue, clearTree } = useBinaryTree();
 
   return (
     <div style={{ minHeight: "100vh" }}>
-      <Header addNode={addNode} removeNode={removeNode} clearTree={clearTree} />
+      <Header
+        addNode={addNode}
+        removeNode={removeNodeByValue}
+        clearTree={clearTree}
+      />
       <div
         id="treeWrapper"
         style={{ display: "flex", flex: 1, height: 500, marginTop: 24 }}
--- src/hooks/use-binary-tree/use-binary-tree.ts
+++ src/hooks/use-binary-tree/use-binary-tree.ts
@@ -1,11 +1,13 @@
+// @ts-nocheck
 import { useState } from "react";
+import _ from "lodash";
 import type { TreeNode, useBinaryTreeParams } from "./use-binary-tree.types";
 
 export const useBinaryTree = (props?: useBinaryTreeParams) => {
   const [tree, setTree] = useState<TreeNode | undefined>(props?.initialState);
 
   const addNode = (value: number) => {
-    const treeClone = tree ? { ...tree } : undefined;
+    const treeClone = tree ? _.cloneDeep(tree) : undefined;
 
     if (!treeClone) {
       setTree({ value });
@@ -36,33 +38,58 @@ export const useBinaryTree = (props?: useBinaryTreeParams) => {
     setTree(treeClone);
   };
 
-  const removeSindleNode = (node: TreeNode) => {
+  const removeNode = (node: TreeNode) => {
     if (!node.leftNode && !node.rightNode) {
       return undefined;
     }
 
-    let newNode = undefined;
+    let currentNode: TreeNode | undefined = _.cloneDeep(node);
+    let parentNode: TreeNode | undefined;
+    let newTree = currentNode;
 
-    if(node.leftNode){
-      newNode = node.leftNode;
-      while(newNode.rightNode !== undefined){
-        newNode = newNode.rightNode
+    if (currentNode.leftNode) {
+      currentNode = currentNode.leftNode;
+      while (currentNode?.rightNode !== undefined) {
+        parentNode = currentNode;
+        currentNode = currentNode.rightNode;
       }
 
-      newNode.rightNode = node.rightNode;
-      return newNode;
+      if (parentNode) {
+        parentNode.rightNode = currentNode.leftNode;
+      } else {
+        newTree.leftNode = currentNode.leftNode;
+      }
+
+      newTree.value = currentNode.value;
+      return newTree;
     }
+
+    // Replace node for another from right side
+
+    currentNode = currentNode.rightNode;
+    while (currentNode?.leftNode !== undefined) {
+      parentNode = currentNode;
+      currentNode = currentNode.leftNode;
+    }
+
+    if (parentNode) {
+      parentNode.leftNode = currentNode?.rightNode;
+    } else {
+      newTree.rightNode = currentNode?.rightNode;
+    }
+
+    newTree.value = currentNode.value;
+    return newTree;
   };
 
-  const removeNode = (value: number) => {
-    const treeClone = tree ? { ...tree } : undefined;
-
+  const removeNodeByValue = (value: number) => {
+    const treeClone = tree ? _.cloneDeep(tree) : undefined;
     if (!treeClone) {
       return;
     }
 
     if (value === treeClone.value) {
-      const newTree = removeSindleNode(treeClone);
+      const newTree = removeNode(treeClone);
       setTree(newTree);
       return;
     }
@@ -70,23 +97,28 @@ export const useBinaryTree = (props?: useBinaryTreeParams) => {
     let currentNode: TreeNode | undefined = treeClone;
 
     while (currentNode !== undefined) {
-      if (value === currentNode.value) {
-        setTree(undefined);
-        break;
-      }
-
       if (value > currentNode.value) {
+        if (currentNode?.rightNode?.value === value) {
+          currentNode.rightNode = removeNode(currentNode.rightNode);
+          break;
+        }
         currentNode = currentNode.rightNode;
         continue;
       }
 
+      if (currentNode?.leftNode?.value === value) {
+        currentNode.leftNode = removeNode(currentNode.leftNode);
+        break;
+      }
       currentNode = currentNode.leftNode;
     }
+
+    setTree(treeClone);
   };
 
-  const clearTree = () =>{
+  const clearTree = () => {
     setTree(undefined);
-  }
+  };
 
-  return { tree, addNode, removeNode, clearTree };
+  return { tree, addNode, removeNodeByValue, clearTree };
 };
